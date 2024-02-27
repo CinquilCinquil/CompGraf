6d9c58349b0b449d8c612c9b935fa9930aee6be5
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-02-27 15:24:44 -0300

    add: + operator overload for vector

--- source/main.cpp
+++ source/main.cpp
@@ -11,6 +11,15 @@ int main() {
 	v.push_back(2);
 	v.push_back(3);	
 	
+	vectoru<int> v2;
+	v2.push_back(10);
+	v2.push_back(20);
+	v2.push_back(30);	
+	
+	cout << v << '\n';
+	
+	v = v2 + v;
+	
 	cout << v << '\n';
 	
 	cout << "THE END\n";
--- source/utils/vectoru.cpp
+++ source/utils/vectoru.cpp
@@ -1,20 +1,23 @@
 #include <vector>
+#include <iostream>
 
 template<class T>
 class vectoru : public std::vector<T> {
 	
 	public:
 	
-	// 
+	//
 };
+
+// Printing
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out,  vectoru<T>& vec) {
 	
-	typename vectoru<T>::iterator it = vec.begin();
-	
 	// printing all of the contents of the vector in the following manner:
 	// [x, y, z, ..., w]
+	
+	typename vectoru<T>::iterator it = vec.begin();
 	
 	out << "[";
 	
@@ -29,4 +32,26 @@ std::ostream& operator<<(std::ostream& out,  vectoru<T>& vec) {
 	
 	return out;
 }
+
+// Arithimetical Operations
+
+template<typename T>
+vectoru<T>& operator+(vectoru<T>& vec_l, vectoru<T>& vec_r) {
+	
+	// adding the contents of vec_r to vec_l
+	
+	typename vectoru<T>::iterator it_l = vec_l.begin();
+	typename vectoru<T>::iterator it_r = vec_r.begin();
+	
+	while (it_l != vec_l.end() && it_r != vec_r.end()) {
+		
+		*it_l = *it_l + *it_r;
+		
+		it_l ++;
+		it_r ++;
+	}
+	
+	return vec_l;
+}
+
 	
