3bbb5850799a82436df437baccb3ef05d2c7f6a7
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-02-27 17:13:28 -0300

    refac: optmized operators

--- source/main.cpp
+++ source/main.cpp
@@ -6,28 +6,32 @@ using namespace std;
 
 int main() {
 	
-	vectoru<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);	
+	vectoru<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);	
 	
 	vectoru<int> v2;
 	v2.push_back(10);
 	v2.push_back(20);
 	v2.push_back(30);
 	
-	vectoru<int> v3 = v * v2;
+	vectoru<int> v3;
 	
+	v3 = v1 + v2;
 	cout << v3 << '\n';
 	
-	cout << v << '\n';
+	v3 = v1 * v2;
+	cout << v3 << '\n';
+	
+	v3 = v1 * 5;
+	cout << v3 << '\n';
+	
+	v3 -= 1;
+	cout << v3 << '\n';
+	
+	cout << v1 << '\n';
 	cout << v2 << '\n';
-	
-	//vectoru<int> v3 = (v + v2);
-	
-	//cout << v3 << '\n';
-	//cout << v << '\n';
-	//cout << v2 << '\n';
 	
 	cout << "THE END\n";
 	
--- source/utils/vectoru.cpp
+++ source/utils/vectoru.cpp
@@ -6,10 +6,29 @@ class vectoru : public std::vector<T> {
 	
 	public:
 	
-	//
+	void fill_with(T a, int n = 1) {
+		(*this).clear();
+		for (int i{0}; i < n;i ++) {
+			(*this).push_back(a);
+		}
+	}
+	
+	vectoru &operator=(const std::initializer_list<T> &il)
+	{
+		typename std::initializer_list<T>::iterator it = il.begin();
+		
+		// Copy the elements from the il into the array.
+		while(it != il.end())
+		{
+			(*this).push_back(*it);
+			it ++;
+		}
+
+		return *this;
+	}
 };
 
-// Printing
+/// Printing
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out,  vectoru<T>& vec) {
@@ -33,107 +52,22 @@ std::ostream& operator<<(std::ostream& out,  vectoru<T>& vec) {
 	return out;
 }
 
-// Arithimetical Operations
+/// Arithimetical Operations
 
 template<typename T>
-vectoru<T> operator+(vectoru<T>& vec_l, vectoru<T>& vec_r) {
+vectoru<T>& operator+=(vectoru<T>& vec_l, vectoru<T>& vec_r) {
 	
-	// v = vec_l + vec_r
-	// v is the size of vec_l !!!
-	
-	vectoru<T> v;
+	// vec_l = vec_l + vec_r
 	
 	typename vectoru<T>::iterator it_l = vec_l.begin();
 	typename vectoru<T>::iterator it_r = vec_r.begin();
 	
 	while (it_l != vec_l.end()) {
 		
-		v.push_back(*it_l + (it_r != vec_r.end() ? *it_r : 0));
+		*it_l = *it_l + (it_r != vec_r.end() ? *it_r : 0);
 		
 		it_l ++;
 		it_r ++;
-	}
-	
-	return v;
-}
-
-template<typename T>
-vectoru<T> operator+(vectoru<T>& vec_l, T a) {
-	
-	// adding 'a' to a copy of the vector
-	
-	vectoru<T> v = vec_l;
-	
-	typename vectoru<T>::iterator it_v = v.begin();
-	
-	while (it_v != v.end()) {
-		
-		*it_v = *it_v + a;
-		
-		it_v ++;
-	}
-	
-	return v;
-}
-
-template<typename T>
-vectoru<T>& operator+=(vectoru<T>& vec_l, T a) {
-	
-	// adding 'a' to the vector
-	
-	typename vectoru<T>::iterator it_l = vec_l.begin();
-	
-	while (it_l != vec_l.end()) {
-		
-		*it_l = *it_l + a;
-		
-		it_l ++;
-	}
-	
-	return vec_l;
-}
-
-template<typename T>
-vectoru<T> operator-(vectoru<T>& vec_l, T a) {
-	return vec_l + -a;
-}
-
-template<typename T>
-vectoru<T>& operator-=(vectoru<T>& vec_l, T a) {
-	return vec_l += -a;
-}
-
-template<typename T>
-vectoru<T> operator*(vectoru<T>& vec_l, T a) {
-	
-	// multiplying a copy of the vector by 'a'
-	
-	vectoru<T> v = vec_l;
-	
-	typename vectoru<T>::iterator it_v = v.begin();
-	
-	while (it_v != v.end()) {
-		
-		*it_v = *it_v * a;
-		
-		it_v ++;
-	}
-	
-	return v;
-}
-
-template<typename T>
-vectoru<T>& operator*=(vectoru<T>& vec_l, T a) {
-	
-	// multiplying the vector by 'a'
-	
-	typename vectoru<T>::iterator it_l = vec_l.begin();
-	
-	while (it_l != vec_l.end()) {
-		
-		*it_l = *it_l * a;
-		
-		it_l ++;
 	}
 	
 	return vec_l;
@@ -159,12 +93,53 @@ vectoru<T>& operator*=(vectoru<T>& vec_l, vectoru<T>& vec_r) {
 }
 
 template<typename T>
+vectoru<T> operator+(vectoru<T>& vec_l, vectoru<T>& vec_r) {
+	vectoru<T> v = vec_l;
+	v += vec_r;
+	return v;
+}
+
+template<typename T>
 vectoru<T> operator*(vectoru<T>& vec_l, vectoru<T>& vec_r) {
-	
-	// multiplying a copy of vec_l by vec_r
-	
 	vectoru<T> v = vec_l;
 	v *= vec_r;
-	
 	return v;
+}
+
+// with numbers
+
+template<typename T>
+vectoru<T>& operator+=(vectoru<T>& vec_l, T a) {
+	vectoru<T> v;
+	v.fill_with(a, vec_l.size());
+	return vec_l += v;
+}
+
+template<typename T>
+vectoru<T> operator+(vectoru<T>& vec_l, T a) {
+	vectoru<T> v = vec_l;
+	return v += a;
+}
+
+template<typename T>
+vectoru<T>& operator-=(vectoru<T>& vec_l, T a) {
+	return vec_l += -a;
+}
+
+template<typename T>
+vectoru<T> operator-(vectoru<T>& vec_l, T a) {
+	return vec_l + -a;
+}
+
+template<typename T>
+vectoru<T>& operator*=(vectoru<T>& vec_l, T a) {
+	vectoru<T> v;
+	v.fill_with(a, vec_l.size());
+	return vec_l *= v;
+}
+
+template<typename T>
+vectoru<T> operator*(vectoru<T>& vec_l, T a) {
+	vectoru<T> v = vec_l;
+	return v *= a;
 }
