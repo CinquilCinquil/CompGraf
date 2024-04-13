d0d485805328858ddf0e5aa078ec661f74299b94
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-04-13 09:32:27 -0300

    fix: declaration of virtual method in material

--- src/core/material.h
+++ src/core/material.h
@@ -9,13 +9,12 @@ class Material {
 public:
     Spectrum color = {255, 0, 0};
 
-    [[nodiscard]] virtual Spectrum sampleUV(const Point2f& pixel_ndc) const;
+    [[nodiscard]] virtual Spectrum sampleUV(const Point2f& pixel_ndc) const = 0;
 
 };
 
-class MatSphereUV : public Material
-{
-    [[nodiscard]] Spectrum sampleUV(const Point2f& pixel_ndc) const override;
+class MatSphereUV : public Material {
+    [[nodiscard]] Spectrum sampleUV(const Point2f& pixel_ndc) const override {return Spectrum{0, 0, 0};};
 };
 
 }
